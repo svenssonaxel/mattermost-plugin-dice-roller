e8c3e715b9124096c129ea87c3d34416f727d62d
Axel Svensson <mail@axelsvensson.com>
2023-04-06 14:42:43 +0200

    Roll comment (NAT1/NAT20)

--- server/dice.go
+++ server/dice.go
@@ -8,17 +8,27 @@ import (
 
 // Types
 type Node struct {
-	token string
-	child []Node
-	sp    NodeSpecialization
+	token       string
+	child       []Node
+	sp          NodeSpecialization
+	rollComment string
 }
 type NodeSpecialization interface {
 	roll(Node, Roller) NodeSpecialization
+	// The roll comment can be
+	// - ROLL_COMMENT_BLOCK_PARENT to indicate that no sum or product
+	//   (grand)parent node is allowed to render a roll comment.
+	// - ROLL_COMMENT_NOTHING to indicate that the node has no roll comment.
+	// - The roll comment as a string, which is only to be used in the case
+	//   that no (grand)parent has one.
+	rollComment(n Node, c configuration) string
 	value(Node) int
 	// The render function takes three arguments:
 	// 1. The node to render.
 	// 2. The indentation prefix, e.g. "- " for the top level.
 	// 3. The result role, one of RR_NONE, RR_TOP, RR_DETAIL.
+	//	  (How the second return value will be used.)
+	// 4. Whether roll comment is allowed to be rendered.
 	// It returns three strings:
 	// 1. An unformatted expression, used as (part of) the left side of the equals sign.
 	// 2. A formatted result potentially used as (part of) the right side of the
@@ -26,7 +36,7 @@ type NodeSpecialization interface {
 	// 3. The details list, i.e. all subsequent rows, formatted and including the
 	//    indentation prefix. The details list either starts with a newline or is the
 	//    empty string.
-	render(n Node, ind string, rr int) (string, string, string)
+	render(n Node, ind string, rr int, rcok bool) (string, string, string)
 }
 type Roller func(int) int
 type GroupExpr struct{}
@@ -61,13 +71,15 @@ const (
 )
 
 // Roller
-func (n Node) roll(roller Roller) Node {
+func (n Node) roll(roller Roller, conf configuration) Node {
 	child := make([]Node, len(n.child))
 	for i, c := range n.child {
-		child[i] = c.roll(roller)
+		child[i] = c.roll(roller, conf)
 	}
 	sp := n.sp.roll(n, roller)
-	return Node{token: n.token, child: child, sp: sp}
+	node := Node{token: n.token, child: child, sp: sp}
+	node.rollComment = sp.rollComment(node, conf)
+	return node
 }
 func (sp GroupExpr) roll(_ Node, _ Roller) NodeSpecialization { return sp }
 func (sp Natural) roll(_ Node, _ Roller) NodeSpecialization   { return sp }
@@ -163,19 +175,26 @@ func (sp CommaList) value(n Node) int {
 }
 
 // Render
+func renderRollComment(n Node, rr int, rcok bool) (string, bool) {
+	if rcok && rr != RR_NONE && n.rollComment != ROLL_COMMENT_NOTHING && n.rollComment != ROLL_COMMENT_BLOCK_PARENT {
+		return n.rollComment, false
+	} else {
+		return "", rcok
+	}
+}
 func (n Node) renderToplevel() string {
-	r1, r2, r3 := n.render("- ", RR_TOP)
+	r1, r2, r3 := n.render("- ", RR_TOP, true)
 	if r2 != "" {
 		return fmt.Sprintf("%s = %s%s", r1, r2, r3)
 	} else {
 		return fmt.Sprintf("%s%s", r1, r3)
 	}
 }
-func (n Node) render(ind string, rr int) (string, string, string) {
-	return n.sp.render(n, ind, rr)
+func (n Node) render(ind string, rr int, rcok bool) (string, string, string) {
+	return n.sp.render(n, ind, rr, rcok)
 }
-func (GroupExpr) render(n Node, ind string, rr int) (string, string, string) {
-	r1, r2, r3 := n.child[0].render(ind, rr)
+func (GroupExpr) render(n Node, ind string, rr int, rcok bool) (string, string, string) {
+	r1, r2, r3 := n.child[0].render(ind, rr, rcok)
 	return fmt.Sprintf("(%s)", r1), r2, r3
 }
 func resultInteger(n int, rr int) string {
@@ -185,22 +204,24 @@ func resultInteger(n int, rr int) string {
 		return fmt.Sprintf("***%d***", n)
 	}
 }
-func (sp Natural) render(_ Node, _ string, rr int) (string, string, string) {
+func (sp Natural) render(_ Node, _ string, rr int, _ bool) (string, string, string) {
 	return fmt.Sprintf("%d", sp.n), resultInteger(sp.n, rr), ""
 }
-func (sp Sum) render(n Node, ind string, rr int) (string, string, string) {
-	return renderSumProd(n, ind, sp.ops, rr)
+func (sp Sum) render(n Node, ind string, rr int, rcok bool) (string, string, string) {
+	return renderSumProd(n, ind, sp.ops, rr, rcok)
 }
-func (sp Prod) render(n Node, ind string, rr int) (string, string, string) {
-	return renderSumProd(n, ind, sp.ops, rr)
+func (sp Prod) render(n Node, ind string, rr int, rcok bool) (string, string, string) {
+	return renderSumProd(n, ind, sp.ops, rr, rcok)
 }
-func renderSumProd(n Node, ind string, ops []string, rr int) (string, string, string) {
+func renderSumProd(n Node, ind string, ops []string, rr int, rcok bool) (string, string, string) {
 	if len(n.child) == 1 {
-		return n.child[0].sp.render(n.child[0], ind, rr)
+		return n.child[0].sp.render(n.child[0], ind, rr, rcok)
 	}
-	r1, r3 := "", ""
+	r1, r2, r3 := "", resultInteger(n.value(), rr), ""
+	rComment, c_rcok := renderRollComment(n, rr, rcok)
+	r2 += rComment
 	for i, c := range n.child {
-		r1a, _, r3a := c.render(ind, RR_NONE)
+		r1a, _, r3a := c.render(ind, RR_NONE, c_rcok)
 		effectiveOp := ops[i]
 		if effectiveOp == "*" {
 			effectiveOp = "×"
@@ -208,9 +229,9 @@ func renderSumProd(n Node, ind string, ops []string, rr int) (string, string, st
 		r1 += effectiveOp + r1a
 		r3 += r3a
 	}
-	return r1, resultInteger(n.value(), rr), r3
+	return r1, r2, r3
 }
-func (sp Dice) render(n Node, ind string, rr int) (string, string, string) {
+func (sp Dice) render(n Node, ind string, rr int, rcok bool) (string, string, string) {
 	needsRollStr := !(sp.n == 1 && len(sp.rolls) == 1 && sp.rolls[0].use)
 	needsDetail := rr == RR_NONE || (rr != RR_DETAIL && needsRollStr)
 	rollStr := ""
@@ -233,9 +254,17 @@ func (sp Dice) render(n Node, ind string, rr int) (string, string, string) {
 	if needsRollStr && !needsDetail {
 		token += rollStr
 	}
-	return token, resultInteger(n.value(), rr), detail
+	result := resultInteger(n.value(), rr)
+	if rr == RR_NONE {
+		rComment, _ := renderRollComment(n, RR_DETAIL, rcok)
+		detail += rComment
+	} else {
+		rComment, _ := renderRollComment(n, RR_DETAIL, rcok)
+		result += rComment
+	}
+	return token, result, detail
 }
-func (sp Stats) render(n Node, ind string, _ int) (string, string, string) {
+func (sp Stats) render(n Node, ind string, _ int, _ bool) (string, string, string) {
 	intro := "up a new character! Adventure awaits. In the meanwhile, here are your ability scores:"
 	// Extract values and sort them descending
 	values := make([]int, len(n.child))
@@ -254,12 +283,12 @@ func (sp Stats) render(n Node, ind string, _ int) (string, string, string) {
 	// Render details
 	details := ""
 	for _, c := range n.child {
-		_, _, detail := c.render(ind, RR_NONE)
+		_, _, detail := c.render(ind, RR_NONE, false)
 		details += detail
 	}
 	return fmt.Sprintf("%s\n%s", intro, scoreText), "", details
 }
-func (sp DeathSave) render(n Node, ind string, rr int) (string, string, string) {
+func (sp DeathSave) render(n Node, ind string, rr int, _ bool) (string, string, string) {
 	event := ""
 	value := n.value()
 	switch {
@@ -272,38 +301,33 @@ func (sp DeathSave) render(n Node, ind string, rr int) (string, string, string) 
 	default:
 		event = "**REGAINS 1 HP!** :star-struck:"
 	}
-	_, _, details := n.child[0].render(ind, RR_NONE)
+	_, _, details := n.child[0].render(ind, RR_NONE, false)
 	return fmt.Sprintf("a death saving throw, and %s", event), "", details
 }
-func (sp Labeled) render(n Node, ind string, rr int) (string, string, string) {
+func (sp Labeled) render(n Node, ind string, rr int, rcok bool) (string, string, string) {
 	if sp.label == "" {
-		return n.child[0].render(ind, rr)
+		return n.child[0].render(ind, rr, rcok)
 	}
+	rComment, c_rcok := renderRollComment(n, rr, rcok)
 	switch rr {
 	case RR_TOP:
-		r1, r2, r3 := n.child[0].render(ind, rr)
-		r2 += fmt.Sprintf(" %s", sp.label)
-		return r1, r2, r3
+		r1, r2, r3 := n.child[0].render(ind, rr, c_rcok)
+		return r1, fmt.Sprintf("%s %s%s", r2, sp.label, rComment), r3
 	case RR_NONE:
-		r1none, _, _ := n.child[0].render("  "+ind, RR_NONE)
-		r1, r2, r3 := n.child[0].render("  "+ind, RR_DETAIL)
-		if r2 != "" {
-			return r1none, r2, fmt.Sprintf("\n%s*%s =* %s *%s*%s", ind, r1, r2, sp.label, r3)
-		} else {
-			return r1none, r2, fmt.Sprintf("\n%s*%s* *%s*%s", ind, r1, sp.label, r3)
-		}
+		r1none, _, _ := n.child[0].render("  "+ind, RR_NONE, false)
+		r1, r2, r3 := n.child[0].render("  "+ind, RR_DETAIL, false)
+		return r1none, r2, fmt.Sprintf("\n%s*%s =* %s *%s*%s%s", ind, r1, r2, sp.label, rComment, r3)
 	case RR_DETAIL:
-		r1, r2, r3 := n.child[0].render("  "+ind, rr)
-		r2 += fmt.Sprintf(" *%s*", sp.label)
-		return r1, r2, r3
+		r1, r2, r3 := n.child[0].render("  "+ind, rr, false)
+		return r1, fmt.Sprintf("%s *%s*%s", r2, sp.label, rComment), r3
 	default:
 		panic("invalid render request in Labeled.render")
 	}
 }
-func (sp CommaList) render(n Node, ind string, rr int) (string, string, string) {
+func (sp CommaList) render(n Node, ind string, rr int, rcok bool) (string, string, string) {
 	r1, r2, r3 := "", "", ""
 	for i, c := range n.child {
-		r1a, r2a, r3a := c.render(ind, rr)
+		r1a, r2a, r3a := c.render(ind, rr, rcok)
 		if i > 0 {
 			r1 += ", "
 			r2 += ", "
@@ -313,4 +337,58 @@ func (sp CommaList) render(n Node, ind string, rr int) (string, string, string) 
 		r3 += r3a
 	}
 	return r1, r2, r3
+}
+
+// roll comment
+const ROLL_COMMENT_BLOCK_PARENT = "<ROLL_COMMENT_BLOCK_PARENT>"
+const ROLL_COMMENT_NOTHING = "<ROLL_COMMENT_NOTHING>"
+
+func (sp GroupExpr) rollComment(n Node, c configuration) string {
+	return n.child[0].sp.rollComment(n.child[0], c)
+}
+func (sp Natural) rollComment(n Node, _ configuration) string { return ROLL_COMMENT_NOTHING }
+func (sp Sum) rollComment(n Node, conf configuration) string  { return rollCommentSumProd(n, conf) }
+func (sp Prod) rollComment(n Node, conf configuration) string { return rollCommentSumProd(n, conf) }
+func rollCommentSumProd(n Node, conf configuration) string {
+	if len(n.child) == 1 {
+		return n.child[0].sp.rollComment(n.child[0], conf)
+	}
+	nofComments := 0
+	comment := ""
+	for _, c := range n.child {
+		cComment := c.sp.rollComment(c, conf)
+		if cComment == ROLL_COMMENT_BLOCK_PARENT {
+			return ROLL_COMMENT_BLOCK_PARENT
+		}
+		if cComment != ROLL_COMMENT_NOTHING {
+			nofComments++
+			comment = cComment
+		}
+	}
+	if nofComments == 1 {
+		return comment
+	}
+	return ROLL_COMMENT_NOTHING
+}
+func (sp Dice) rollComment(n Node, conf configuration) string {
+	if conf.EnableDnd5e && sp.x == 20 && (sp.h-sp.l) == 1 {
+		if n.value() == 20 {
+			return " (NAT20! :star-struck:)"
+		}
+		if n.value() == 1 {
+			return " (NAT1! :grimacing:)"
+		}
+	}
+	return ROLL_COMMENT_BLOCK_PARENT
+}
+func (sp Stats) rollComment(n Node, _ configuration) string     { return ROLL_COMMENT_NOTHING }
+func (sp DeathSave) rollComment(n Node, _ configuration) string { return ROLL_COMMENT_NOTHING }
+func (sp Labeled) rollComment(n Node, conf configuration) string {
+	return n.child[0].sp.rollComment(n.child[0], conf)
+}
+func (sp CommaList) rollComment(n Node, conf configuration) string {
+	if len(n.child) == 1 {
+		return n.child[0].sp.rollComment(n.child[0], conf)
+	}
+	return ROLL_COMMENT_NOTHING
 }
